8e3ab603ae52d96177ed8423f3634b44dd863333
froggagul <froggagul@gmail.com>
2025-03-13 22:36:52 +0900

    feat: data loader

--- src/ddp/utils.py
+++ src/ddp/utils.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from typing import Optional
 import pinocchio as pin
 from pinocchio.robot_wrapper import RobotWrapper
-
+import joblib
 """
     G1 loading based on https://github.com/Gepetto/example-robot-data
 """
@@ -29,7 +29,7 @@ class G1Loader:
                 self.model_path,
                 pin.JointModelFreeFlyer() if self.free_flyer else None,
             )
-        
+
         self.srdf_path = None
         self.robot.q0 = pin.neutral(self.robot.model)
         self.robot.urdf = urdf_path
@@ -44,3 +44,70 @@ class G1Loader:
         lb = self.robot.model.lowerPositionLimit
         lb[:7] = -1
         self.robot.model.lowerPositionLimit = lb
+
+def get_data(data_path: str):
+    link_names = {
+        "left_hip_pitch_joint": 1,
+        "left_hip_roll_joint": 1,
+        "left_hip_yaw_joint": 1,
+        "left_knee_joint": 4,
+        "left_ankle_pitch_joint": 7,
+        "left_ankle_roll_joint": 7,
+        "right_hip_pitch_joint": 2,
+        "right_hip_roll_joint": 2,
+        "right_hip_yaw_joint": 2,
+        "right_knee_joint": 5,
+        "right_ankle_pitch_joint": 8,
+        "right_ankle_roll_joint": 8,
+        "waist_yaw_joint": 0,
+        "waist_roll_joint": 0,
+        "waist_pitch_joint": 0,
+        "left_shoulder_pitch_joint": 16,
+        "left_shoulder_roll_joint": 16,
+        "left_shoulder_yaw_joint": 16,
+        "left_elbow_joint": 18,
+        "right_shoulder_pitch_joint": 17,
+        "right_shoulder_roll_joint": 17,
+        "right_shoulder_yaw_joint": 17,
+        "right_elbow_joint": 19,
+    }
+    smpl_joint_names = [
+        "PELVIS", 
+        "L_HIP", 
+        "R_HIP", 
+        "SPINE1", 
+        "L_KNEE", 
+        "R_KNEE", 
+        "SPINE2", 
+        "L_ANKLE", 
+        "R_ANKLE", 
+        "SPINE3", 
+        "L_FOOT", 
+        "R_FOOT", 
+        "NECK", 
+        "L_COLLAR", 
+        "R_COLLAR", 
+        "HEAD", 
+        "L_SHOULDER", 
+        "R_SHOULDER", 
+        "L_ELBOW", 
+        "R_ELBOW", 
+        "L_WRIST", 
+        "R_WRIST", 
+        "L_HAND", 
+        "R_HAND", 
+    ]
+    with open(data_path, "rb") as f:
+        data = joblib.load(f)
+    smpl_joints = data['smpl_joints']
+    print("matched keys")
+    for link_name, smpl_idx in link_names.items():
+        print(f"{link_name}: {smpl_joint_names[smpl_idx]}")
+    ret = {link_name: smpl_joints[:, link_id, :] for link_name, link_id in link_names.items()}
+    return ret
+        
+
+if __name__ == "__main__":
+    data_path = "/root/ddp-cr7/data/traj/ddp_cr7_data.pkl"
+    data = get_data(data_path)
+    print(data.keys())
